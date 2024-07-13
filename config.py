95990632f096ff9ee3051d94bd637b3a154a1867
THE_80X <ivansolovev1488@gmail.com>
2024-07-13 13:31:10 +0300

    S3P Plugin for SWIFT API source

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from s3p_sdk.plugin.config import (
 )
 from s3p_sdk.plugin.config.type import SOURCE
 from s3p_sdk.module import (
-    WEBDRIVER,
+    WebDriver,
 )
 
 config = PluginConfig(
@@ -41,7 +41,7 @@ config = PluginConfig(
         entry=payload.entry.EntryConfig(
             method='content',
             params=[
-                payload.entry.ModuleParamConfig(key='driver', module_name=WEBDRIVER, bus=True),
+                payload.entry.ModuleParamConfig(key='driver', module_name=WebDriver, bus=True),
                 payload.entry.ConstParamConfig(key='max_count_documents', value=50),
                 payload.entry.ConstParamConfig(key='url', value='https://developer.swift.com/apis'),
                 payload.entry.ConstParamConfig(key='timeout', value=20)
